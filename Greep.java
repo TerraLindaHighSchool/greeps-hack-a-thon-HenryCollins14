e4a8c930edffb7ff3bcc7242a633f3d4f6cf8b36
HenryCollins14 <60672325+HenryCollins14@users.noreply.github.com>
2020-03-12 13:38:16 -0700

    Day 2 of Greep

--- Greep.java
+++ Greep.java
@@ -11,6 +11,11 @@ public class Greep extends Creature
     // Remember: you cannot extend the Greep's memory. So:
     // no additional fields (other than final fields) allowed in this class!
     
+    private boolean goHome = false; 
+    public boolean flag1 = false; 
+    public boolean flag2 = false; 
+    public final int HOW_MANY_STEPS = 3;
+    
     /**
      * Default constructor for testing purposes.
      */
@@ -33,52 +38,105 @@ public class Greep extends Creature
     public void act()
     {
         super.act();   // do not delete! leave as first statement in act().
-        if (carryingTomato()) {
+        if (carryingTomato()) 
+        {
+            int remainingSteps = getMemory();
+            if(remainingSteps == 0)
+            {
+                spit("purple");
+                turnHome();
+            }
+            else
+            {
+                remainingSteps--;
+                setMemory(remainingSteps);
+            }
+           
             if (atShip()) {
                 dropTomato();
+                turn(180);
             }
             else {
-                turnHome();
                 move();
+                goHome = true;
+            }
+            if (atWater() || isAtEdge())
+            {
+                turn(Greenfoot.getRandomNumber(65));
+                setMemory(HOW_MANY_STEPS + Greenfoot.getRandomNumber(HOW_MANY_STEPS));
             }
         }
-        else 
-        {
+        else {
             checkFood();
-            atWorldEdge();
+            //spit("red");
         }
+        
+        
+        if(atWater() || isAtEdge())
+        {
+            turn(Greenfoot.getRandomNumber(80));
+            move();
+            
+        }
+        
     }
     
     /**
-    * Is there any food here where we are? If so, try to load some!
+     * Is there any food here where we are? If so, try to load some!
      */
     public void checkFood()
     {
         // check whether there's a tomato pile here
         TomatoPile tomatoes = (TomatoPile) getOneIntersectingObject(TomatoPile.class);
-        if (tomatoes != null) {
-            callGreeps();
+        Greep greeps = (Greep) getOneIntersectingObject(Greep.class);
+        if (tomatoes != null) 
+        {
             loadTomato();
+            
+            
+            // Note: this attempts to load a tomato onto *another* Greep. It won't
+            // do anything if we are alone here.
         }
         else
         {
             move();
+            
+            
         }
     }
     
+    public void moveToFlag()
+    {
+        TomatoPile tomatoes = (TomatoPile) getOneIntersectingObject(TomatoPile.class);
+        if (tomatoes != null)
+        {
+            setFlag(1, true);
+            flag1 = true;
+        }
+        if(tomatoes != null && flag1 == true)
+        {
+            setFlag(2, true); 
+            flag2 = true;
+        }
+        
+    }
+    
+    /**
+     * When a greep is on a tomato patch, the greep will call the other greeps to itself
+     * only greeps in a certain area are called.
+     */
     public void callGreeps()
     {
-        int dX = this.getX() - getX();
-        int dY = this.getY() - getY();
-        setRotation((int) (180 * Math.atan2(dY, dX) / Math.PI));
+        //int dX = Creature.getX() - getX();
+        //int dY = Creature.getY() - getY();
+        //setRotation((int) (180 * Math.atan2(dY, dX) / Math.PI));
     }
-
     /**
      * This method specifies the name of the author (for display on the result board).
      */
     public static String getAuthorName()
     {
-        return "Anonymous";  // write your name here!
+        return "Henry Collins";  // write your name here!
     }
 
     /**
@@ -94,4 +152,6 @@ public class Greep extends Creature
             return "greep.png";
         }
     }
+    
+    
 }
